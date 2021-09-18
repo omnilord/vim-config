45863de8b9965083e7b33c3cdb709446f5926140
Eric L. Truitte, MBA <omnilord@privateerconsulting.com>
2021-09-18 01:29:10 -0400

    Sets up submodules

--- install.sh
+++ install.sh
@@ -6,17 +6,17 @@ BASH_CONFIG_FILE="${BASH_CONFIG_FILE:=~/.bashrc}"
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-rails.git
-git clone git://github.com/vim-ruby/vim-ruby.git
-git clone git://github.com/tomtom/tlib_vim.git
-git clone git://github.com/MarcWeber/vim-addon-mw-utils.git
-git clone git://github.com/garbas/vim-snipmate.git
-git clone git://github.com/honza/vim-snippets.git
-git clone git://github.com/tpope/vim-endwise.git
-git clone git://github.com/scrooloose/nerdtree.git
-git clone git://github.com/Xuyuanp/nerdtree-git-plugin.git
-git clone git://github.com/ekalinin/Dockerfile.vim.git
+# The old way before setting up submodules
+# cd ~/.vim/bundle
+# git clone git://github.com/tpope/vim-rails.git
+# git clone git://github.com/vim-ruby/vim-ruby.git
+# git clone git://github.com/tomtom/tlib_vim.git
+# git clone git://github.com/MarcWeber/vim-addon-mw-utils.git
+# git clone git://github.com/garbas/vim-snipmate.git
+# git clone git://github.com/honza/vim-snippets.git
+# git clone git://github.com/tpope/vim-endwise.git
+# git clone git://github.com/scrooloose/nerdtree.git
+# git clone git://github.com/Xuyuanp/nerdtree-git-plugin.git
 
 echo -e "\nexport VIMINIT='source \$MYVIMRC'" >> $BASH_CONFIG_FILE
 echo "export MYVIMRC='~/.vim/.vimrc'" >> $BASH_CONFIG_FILE
