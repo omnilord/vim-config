d9506b4f20633ed205405caafbcbf714c5f1fd61
Eric L. Truitte, MBA <omnilord@privateerconsulting.com>
2024-01-23 11:26:59 -0500

    Replace in-line pulls with git modules for extension bundles

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cp -R bundle bundle_bu_$(date "+%m%d%H%M%Y.%S")
+#cp -R bundle bundle_bu_$(date "+%m%d%H%M%Y.%S")
 
-for d in bundle/*
-do
-  ( cd "${d}" && echo && echo "updating ${d}" && git pull origin master )
-done
+#for d in bundle/*
+#do
+#  ( cd "${d}" && echo && echo "updating ${d}" && git pull origin master )
+#done
+#
+
+git submodule update --remote
+
